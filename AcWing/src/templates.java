91262c801f81f8331c22c7bac66740a4b16fe811
optimjie <1623287075@qq.com>
2021-07-21 09:37:42 +0800

    Java输入输出模板

---
+++ AcWing/src/templates.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class templates {
+    public static void main(String[] args) throws IOException {
+        StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+    }
+}
