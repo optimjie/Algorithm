73389129032715bd21d60aec29c81d1bf5ffccf8
optimjie <1623287075@qq.com>
2021-07-27 20:20:21 +0800

    671. 二叉树中第二小的节点

---
+++ LeetCode/src/com/leetcode/problem/_671二叉树中第二小的节点/Solution.java
@@ -0,0 +1,30 @@
+package com.leetcode.problem._671二叉树中第二小的节点;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    public class TreeNode {
+        TreeNode left, right;
+        int val;
+    }
+    public List<Integer> l = new ArrayList<>();
+    public int findSecondMinimumValue(TreeNode root) {
+        dfs(root);
+        Collections.sort(l);
+        for (int i = 1; i < l.size(); i++) {
+            if (l.get(i) > l.get(0)) {
+                return l.get(i);
+            }
+        }
+        return -1;
+    }
+    public void dfs(TreeNode r) {
+        if (r == null) return;
+        dfs(r.left);
+        l.add(r.val);
+        dfs(r.right);
+    }
+}
