2cdbb577a00cfcdb55726ca3c9a5ca371b660f11
optimjie <1623287075@qq.com>
2021-08-10 15:24:04 +0800

    2021.8.10

--- LeetCode/src/com/leetcode/DailyChallenge/_2021_8/_10_413等差数列划分.java
+++ LeetCode/src/com/leetcode/DailyChallenge/_2021_8/_10_413等差数列划分.java
@@ -28,4 +28,25 @@ public class _10_413等差数列划分 {
         }
         return ans;
     }
+    /**
+     * @author: 李元杰
+     * @params: [nums]
+     * @description: 动态规划，f[i]表示以i为结尾的等差数列的个数，状态转移：如果三个数的不构成等差数列则f[i] = 0
+     *               否则f[i]只比f[i - 1]多一个，也就是nums[i - 2], nums[i - 1], nums[i]
+     * @date: 15:20 2021-08-10
+     * @return: int
+     */
+    public static int numberOfArithmeticSlices1(int[] nums) {
+        int n = nums.length;
+        if (n < 3) return 0;
+        int[] f = new int[n];
+        int ans = 0;
+        for (int i = 2; i < n; i++) {
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                f[i] = f[i - 1] + 1;
+                ans += f[i];
+            }
+        }
+        return ans;
+    }
 }
