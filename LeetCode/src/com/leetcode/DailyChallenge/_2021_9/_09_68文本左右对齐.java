7ca26d4d772eb37d835e5e2df05ec86c716163aa
optimjie <1623287075@qq.com>
2021-09-09 10:28:43 +0800

    2021.9.9

--- LeetCode/src/com/leetcode/DailyChallenge/_2021_9/_09_68文本左右对齐.java
+++ LeetCode/src/com/leetcode/DailyChallenge/_2021_9/_09_68文本左右对齐.java
@@ -8,15 +8,6 @@ public class _09_68文本左右对齐 {
         List<String> ans = new ArrayList<>();
         int n = words.length;
         for (int l = 0; l < n; ) {
-            if (l == n - 1) {
-                String t = words[l];
-                int sub = maxWidth - words[l].length();
-                while (sub-- > 0) {
-                    t = t + " ";
-                }
-                ans.add(t);
-                break;
-            }
             int len = words[l].length() + 1, r = l + 1;
             while (r < n && len + words[r].length() + 1 <= maxWidth + 1) {
                 len += words[r].length() + 1;
@@ -33,33 +24,33 @@ public class _09_68文本左右对齐 {
                     t = t + " ";
                 }
                 ans.add(t);
+                break;
+            }
+            int cnt = r - l; // 字符串数量
+            int len1 = 0;
+            for (int i = l; i < r; i++) len1 += words[i].length();
+            int sub = maxWidth - len1;
+            int[] blankNo = new int[cnt - 1];
+            if (cnt == 1) { // 特判一行只有一个单词的情况
+                String t = words[l];
+                int kk = maxWidth - words[l].length();
+                while (kk-- > 0) {
+                    t = t + " ";
+                }
+                ans.add(t);
             } else {
-                int cnt = r - l; // 字符串数量
-                int len1 = 0;
-                for (int i = l; i < r; i++) len1 += words[i].length();
-                int sub = maxWidth - len1;
-                int[] blankNo = new int[cnt - 1];
-                if (cnt == 1) { // 特判一行只有一个单词的情况
-                    String t = words[l];
-                    int kk = maxWidth - words[l].length();
-                    while (kk-- > 0) {
-                        t = t + " ";
-                    }
-                    ans.add(t);
-                } else {
-                    calculateBlank(blankNo, sub, cnt - 1); // 2 0
-                    String t = "";
-                    for (int i = l, j = 0; i < r; i++, j++) {
-                        t = t + words[i];
-                        if (i != r - 1) {
-                            int sum = blankNo[j];
-                            while (sum -- > 0) {
-                                t = t + " ";
-                            }
+                calculateBlank(blankNo, sub, cnt - 1);
+                String t = "";
+                for (int i = l, j = 0; i < r; i++, j++) {
+                    t = t + words[i];
+                    if (i != r - 1) {
+                        int sum = blankNo[j];
+                        while (sum -- > 0) {
+                            t = t + " ";
                         }
                     }
-                    ans.add(t);
                 }
+                ans.add(t);
             }
             l = r;
         }
